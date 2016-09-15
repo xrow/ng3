df47ee472746c1152e3d85622113c1053148472a
xrow <bjoern@xrow.de>
2016-09-15 18:56:27 +0200

    add apps that are not loaded

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import { App2Component } from './app/app2';
+import { AdServingComponent } from './app/ad-serving';
 
 if (environment.production) {
   enableProdMode();
 }
 //test
 platformBrowserDynamic().bootstrapModule(AppModule);
+//platformBrowserDynamic().bootstrapModule(App2Component);
+//platformBrowserDynamic().bootstrapModule(AdServingComponent);
+//platformBrowserDynamic(App2Component);
+//platformBrowserDynamic(AdServingComponent);
